741116f269bd70ab61b7b90320b5400776db7b38
wenxuan-xia <wenxuan.xia@hotmail.com>
2015-02-16 00:57:27 +0800

    modify hw2.js

--- hw2_script.js
+++ hw2_script.js
@@ -1,47 +1,47 @@
 $(document).ready(function(){
+	var a = document.getElementById('table')
 	var url="https://hivelab.org/static/students.json";
-     $.getJSON(url, function(info){
-     		var a = $(".tbody");
-     	for(var i=0;i<info.length;i++)
-     		{	
-     		a.append("<tr>");
-     			info[i].Total=(info[i].GPA)/4*100+(info[i].GRE_V)/170*100+(info[i].GRE_Q)/170*100+(info[i].Essay)/10*100+(info[i].Recom)/10*100;
-				a.append("<td>"+info[i].Name+"</td>");
-				a.append("<td>"+info[i].GRE_V+"</td>");
-				a.append("<td>"+info[i].GRE_Q+"</td>");
-				a.append("<td>"+info[i].Essay+"</td>");
-				a.append("<td>"+info[i].Recom+"</td>");
-				a.append("<td>"+info[i].Total+"</td>");
-				a.append("</tr>");
-     		
-				 info[i].mark=i
-     		}
-				
-				
-				{
-					for ( i=0; i<info.length; i++) {
-		                            for (var j=i+1; j<info.length; j++) {
-			                   if (info[i].total < info[j].total) {
-				                     t = info[i];
-				                    info[i] = info[j];
-				                   info[j] = t;
-		                           	}
-		                             }
-        	                         }
-	                for ( i=0; i<info.length; i++) {
-		               info[i].Ranking = i+1;
-                                 	}
-	                for ( i=0; i<info.length; i++) {
-	                   	for ( j=i+1; j<info.length; j++) {
-			               if (info[i].mark > info[j].mark) {
-			             	t = info[i];
-				        info[i] = info[j];
-				        info[j] = t;
-		                      	}
-		                 }
-                    	}
+     d3.json(url, function(info){
+     	
+     	for (var i=0; i<info.length; i++) {
+     		info[i].Total=(info[i].GPA)/4*100+(info[i].GRE_V)/170*100+(info[i].GRE_Q)/170*100+(info[i].Essay)/10*100+(info[i].Recom)/10*100;
+     		info[i].mark = i;
+     	}
+						
+		for ( i=0; i<info.length; i++) {
+            for (var j=i+1; j<info.length; j++) {
+                   if (info[i].Total < info[j].Total) {
+	                    t = info[i];
+	                    info[i] = info[j];
+	                    info[j] = t;
+                    }
+         	}
+         }
+        for ( i=0; i<info.length; i++) {
+           info[i].Ranking = i+1;
+            }
+        for ( i=0; i<info.length; i++) {
+           	for ( j=i+1; j<info.length; j++) {
+               if (info[i].mark > info[j].mark) {
+             	t = info[i];
+	        info[i] = info[j];
+	        info[j] = t;
+                  	}
+             }
+        	}
 
-     };
+     var a = document.getElementById('table')
+     	for (var i = 0; i<info.length; i++) {
+			tr = a.insertRow()
+			tr.insertCell().innerHTML = info[i].Name
+			tr.insertCell().innerHTML = info[i].GPA
+			tr.insertCell().innerHTML = info[i].GRE_V
+			tr.insertCell().innerHTML = info[i].GRE_Q
+			tr.insertCell().innerHTML = info[i].Essay
+			tr.insertCell().innerHTML = info[i].Recom
+			tr.insertCell().innerHTML = info[i].Total.toFixed(4)
+			tr.insertCell().innerHTML = info[i].Ranking
+		};
      for( i=0;i<info.length;i++){
      	a.append("<tr>");a.append("<td>"+info[i].Ranking+"</td>");	a.append("</tr>");}
      
